263339a2b0a8a1c29d0a3c46e2b2ddbbc00c6dcd
Narek003 <narek_eritsyan@mail.ru>
2022-10-11 21:29:14 +0400

    Last changes

--- script.js
+++ script.js
@@ -1,113 +1,95 @@
-let table = document.querySelector("table")
+const table = document.querySelector("table")
+const gameTable = document.querySelector(".gameTable")
+const xH = document.getElementById("xH")
+const oH = document.getElementById("oH")
 let gameArr = []
 let player
 let xWins = 0
 let oWins = 0
-let xH = document.getElementById("xH")
-let oH = document.getElementById("oH")
-let interval
 
 function createPlayZone(){
-    clearInterval(interval)
     player = "X"
     for(let i = 0; i < 3; i++){
-        let row = document.createElement("tr")
+        const row = document.createElement("tr")
         gameArr.push([])
         for(let j = 0; j < 3; j++){
-            let col = document.createElement("td")
+            const col = document.createElement("td")
             col.id = `id${i}${j}`
+            col.addEventListener('click', (ev) => {
+                if(ev.target.innerHTML == ""){
+                    ev.target.innerHTML = `${player}`
+                    soundClick()
+                    gameArr[i][j] = player
+                    if(player == "X"){
+                        player = "O"
+                        ev.target.style.color = "#22df13"
+                    }else player = "X"
+                }
+                if(checkWinner([0, 0], [0, 1], [0, 2]) ||
+                checkWinner([1, 0], [1, 1], [1, 2]) ||
+                checkWinner([2, 0], [2, 1], [2, 2]) ||
+                checkWinner([0, 0], [1, 0], [2, 0]) ||
+                checkWinner([0, 1], [1, 1], [2, 1]) ||
+                checkWinner([0, 2], [1, 2], [2, 2]) ||
+                checkWinner([0, 0], [1, 1], [2, 2]) ||
+                checkWinner([2, 0], [1, 1], [0, 2])) {}
+                else if(gameArr.flat().length == 9) draw()
+            })
             row.append(col)
-            gameArr[i].push(col.id)
         }
         table.append(row)
     }
 }
-function turn(){
-    table.addEventListener("click", (td) => {
-        if(td.target.innerHTML == ""){
-            td.target.innerHTML = `${player}`
-            soundClick()
-            if(player == "X"){
-                player = "O"
-                td.target.style.color = "#22df13"
-            }else{
-                player = "X"
-            }
-        }
-        checkWinner()
-    })
-}
-createPlayZone()
-turn()
 
-function checkWinner(){
-    let i = 1
-    let j = 1
-    
-    if((document.getElementById(gameArr[i][j]).innerHTML == document.getElementById(gameArr[i][j - 1]).innerHTML && document.getElementById(gameArr[i][j]).innerHTML == document.getElementById(gameArr[i][j + 1]).innerHTML && document.getElementById(gameArr[i][j]).innerHTML != "") ||
-    (document.getElementById(gameArr[i][j]).innerHTML == document.getElementById(gameArr[i - 1][j]).innerHTML && document.getElementById(gameArr[i][j]).innerHTML == document.getElementById(gameArr[i + 1][j]).innerHTML && document.getElementById(gameArr[i][j]).innerHTML != "") ||
-    (document.getElementById(gameArr[i][j]).innerHTML == document.getElementById(gameArr[i - 1][j - 1]).innerHTML && document.getElementById(gameArr[i][j]).innerHTML == document.getElementById(gameArr[i + 1][j + 1]).innerHTML && document.getElementById(gameArr[i][j]).innerHTML != "") ||
-    (document.getElementById(gameArr[i][j]).innerHTML == document.getElementById(gameArr[i - 1][j + 1]).innerHTML && document.getElementById(gameArr[i][j]).innerHTML == document.getElementById(gameArr[i + 1][j - 1]).innerHTML && document.getElementById(gameArr[i][j]).innerHTML != "")){
-        winSound()
-        if(document.getElementById(gameArr[i][j]).innerHTML == "X"){
-            xWins++
-            xH.innerText = `X: ${xWins}`
-        }else{
-            oWins++
-            oH.innerText = `O: ${oWins}`
-        }
-        interval = setInterval(()=>{
-            table.innerHTML = ""
-            createPlayZone()
-        },3000)
-    }else 
-    if((document.getElementById(gameArr[i - 1][j - 1]).innerHTML == document.getElementById(gameArr[i - 1][j]).innerHTML && document.getElementById(gameArr[i - 1][j - 1]).innerHTML == document.getElementById(gameArr[i - 1][j + 1]).innerHTML &&document.getElementById(gameArr[i - 1][j - 1]).innerHTML != "") ||
-    (document.getElementById(gameArr[i - 1][j - 1]).innerHTML == document.getElementById(gameArr[i][j - 1]).innerHTML && document.getElementById(gameArr[i - 1][j - 1]).innerHTML == document.getElementById(gameArr[i + 1][j - 1]).innerHTML && document.getElementById(gameArr[i - 1][j - 1]).innerHTML != "")){
-        winSound()
-        if(document.getElementById(gameArr[i - 1][j - 1]).innerHTML == "X") {
-            xWins++
-            xH.innerText = `X: ${xWins}`
-        }else{
-            oWins++
-            oH.innerText = `O: ${oWins}`
-        }
-        interval = setInterval(()=>{
-                table.innerHTML = ""
-                createPlayZone()
-        },3000)
-    }else
-    if((document.getElementById(gameArr[i + 1][j + 1]).innerHTML == document.getElementById(gameArr[i + 1][j]).innerHTML && document.getElementById(gameArr[i + 1][j + 1]).innerHTML == document.getElementById(gameArr[i + 1][j - 1]).innerHTML && document.getElementById(gameArr[i + 1][j + 1]).innerHTML != "") ||
-    (document.getElementById(gameArr[i + 1][j + 1]).innerHTML == document.getElementById(gameArr[i][j + 1]).innerHTML && document.getElementById(gameArr[i + 1][j + 1]).innerHTML == document.getElementById(gameArr[i - 1][j + 1]).innerHTML &&document.getElementById(gameArr[i + 1][j + 1]).innerHTML != "")){
-        winSound()
-        if(document.getElementById(gameArr[i + 1][j + 1]).innerHTML == "X") {
-            xWins++
-            xH.innerText = `X: ${xWins}`
-        }else{
-            oWins++
-            oH.innerText = `O: ${oWins}`
-        }
-        interval = setInterval(()=>{
-            table.innerHTML = ""
-            createPlayZone()
-        },3000)
-    }else
-    if(document.getElementById(gameArr[i - 1][j - 1]).innerHTML != "" && 
-    document.getElementById(gameArr[i - 1][j]).innerHTML != "" && 
-    document.getElementById(gameArr[i - 1][j + 1]).innerHTML != "" && 
-    document.getElementById(gameArr[i][j - 1]).innerHTML != "" && 
-    document.getElementById(gameArr[i][j]).innerHTML != "" && 
-    document.getElementById(gameArr[i][j + 1]).innerHTML != "" && 
-    document.getElementById(gameArr[i + 1][j - 1]).innerHTML != "" && 
-    document.getElementById(gameArr[i + 1][j]).innerHTML != "" && 
-    document.getElementById(gameArr[i + 1][j + 1]).innerHTML != "") {
-        loseSound()
-        interval = setInterval(()=>{
-            table.innerHTML = ""
-            createPlayZone()
-        },3000)
+createPlayZone()
+
+function checkWinner(x1, x2, x3){
+    if(gameArr[x1[0]][x1[1]] == gameArr[x2[0]][x2[1]] &&
+        gameArr[x1[0]][x1[1]] == gameArr[x3[0]][x3[1]] &&
+        gameArr[x1[0]][x1[1]] != undefined) {
+            win(gameArr[x1[0]][x1[1]])
+            return true
     }
 }
 
+function draw(){
+    drawSound()
+    const backDiv = document.createElement('div')
+    backDiv.className = 'backDiv'
+    gameTable.append(backDiv)
+    setTimeout(()=>{
+        gameArr = []
+        table.innerHTML = ""
+        backDiv.remove()
+        createPlayZone()
+    },3000)
+}
+
+function win(mark){
+    winSound()
+    let forStyle
+    if(mark == "X"){
+        xWins++
+        xH.innerText = `X: ${xWins}`
+        xH.style.transform = 'scale(2)'
+        forStyle = 'X'
+    }else{
+        oWins++
+        oH.innerText = `O: ${oWins}`
+        oH.style.transform = 'scale(2)'
+        forStyle = 'O'
+    }
+    const backDiv = document.createElement('div')
+    backDiv.className = 'backDiv'
+    gameTable.append(backDiv)
+    setTimeout(()=>{
+        gameArr = []
+        forStyle === 'X' ? xH.style.transform = 'scale(1)' : oH.style.transform = 'scale(1)'
+        table.innerHTML = ""
+        backDiv.remove()
+        createPlayZone()
+    },3000)
+}
 
 function soundClick() {
     let audio = new Audio(); 
@@ -119,7 +101,7 @@ function winSound() {
     audio.src = "audio/winSound.mp3"
     audio.autoplay = true
 }
-function loseSound() {
+function drawSound() {
     let audio = new Audio()
     audio.src = "audio/loseSound.wav"
     audio.autoplay = true
